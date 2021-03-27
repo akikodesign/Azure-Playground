c4e9be447929a9b17689ff865202c542402ad6bb
siva@akikodesign.com <siva@akikodesign.com>
2021-03-27 17:30:23 +0000

    test

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router()
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,4 +7,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-xxx
